3fc0506bda6e5e4d725522fa65374e5ea1132ee6
Kim Jun Sik <jinaganda5@gmail.com>
2019-05-18 19:15:55 +0900

    Making a To Do List part Two
    filter && forEach 중요해!

--- todolist.js
+++ todolist.js
@@ -4,15 +4,41 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   delBtn.innerText = "💔";
+  delBtn.addEventListener("click", deleteToDo);
   const span = document.createElement("span");
   span.innerText = text;
+  const newId = toDos.length + 1;
   li.appendChild(span);
   li.appendChild(delBtn);
+  li.id = newId;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handleSumit(event) {
@@ -22,9 +48,15 @@ function handleSumit(event) {
   toDoInput.value = "";
 }
 
+function some(toDo) {
+  paintToDo(toDo.text);
+}
+
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parseToDos = JSON.parse(loadedToDos);
+    parseToDos.forEach(some);
   }
 }
 
