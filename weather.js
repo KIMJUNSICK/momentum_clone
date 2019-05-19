db4baf54c6fbd96ccc3a1ded316df29e4ee72ece
Kim Jun Sik <jinaganda5@gmail.com>
2019-05-19 10:13:40 +0900

    Getting the Weather part Two (Geolocation)

---
+++ weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "9df02812a15dc211a55a85c929651b05";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("Cant access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCords = localStorage.getItem(COORDS);
+  if (loadedCords === null) {
+    askForCoords();
+  } else {
+    //getWeather
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
